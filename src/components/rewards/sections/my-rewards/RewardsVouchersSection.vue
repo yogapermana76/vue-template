99adf631ecf8e3d090b581a98e7785cbff1ada6d
Yoga Permana <permana76.yp@gmail.com>
2026-04-20 22:45:02 +0700

    refactor: remove unused watch import from RewardsVouchersSection.vue

--- src/components/rewards/sections/my-rewards/RewardsVouchersSection.vue
+++ src/components/rewards/sections/my-rewards/RewardsVouchersSection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { ref, computed, watch } from 'vue'
+  import { ref, computed } from 'vue'
   import { useRouter } from 'vue-router'
   import { useIntersectionObserver } from '@vueuse/core'
   import {
